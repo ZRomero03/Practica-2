69fb05f9690eaf03f7754ac329102f95ea454127
ZRomero03 <143622076+ZRomero03@users.noreply.github.com>
2023-09-08 08:42:14 -0600

    Add files via upload

---
+++ areaTriangulo1.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author Usuario
+ */
+public class areaTriangulo {
+   
+    public static void main(String[] args) {
+        //numeros enteros
+        //numeros decimales
+        
+        int base = 15;
+        int altura = 10;
+        
+        int multiplicacion = 15 * 10;
+        int area = multiplicacion / 2;
+        System.out.println("El área es:" + area);
+        
+    }
+}
