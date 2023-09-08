e0cdcc55c69f7e16476643827d76b6a5ad9c6ded
ZRomero03 <143622076+ZRomero03@users.noreply.github.com>
2023-09-07 23:16:08 -0600

    Add files via upload

---
+++ Listadetareas.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.project1;
+
+
+public class ListaTareas
+{
+ 
+    public static void main(String[] args)
+    {
+       String T1 = " trastes ";
+       String T2 = " ropa ";
+        String T3 = " cama ";
+        String T4 = " casa ";
+        String T5 = " casa ";
+        System.out.println(" Tarea 1:" + T1);
+        System.out.println(" Tarea 2: " + T2);
+        System.out.println(" Tarea 3: " + T3);
+        System.out.println(" Tarea 4: " +T4);
+        System.out.println(" Tarea 5: " +T5);
+        
+    }
+}
